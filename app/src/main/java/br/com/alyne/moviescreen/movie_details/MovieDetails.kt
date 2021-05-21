90677c74932ded935db5aac0908f47fe5f25cc4f
Alyne <42252701+alynensilva@users.noreply.github.com>
2021-05-21 16:06:00 -0300

    Initial commit

---
+++ app/src/main/java/br/com/alyne/moviescreen/movie_details/MovieDetails.kt
@@ -0,0 +1,11 @@
+package br.com.alyne.moviescreen.movie_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
